3ea5d55b1a9e812d0f5769d623b0101fe803ef2b
naturexie <786281870@qq.com>
2025-04-26 18:21:09 +0800

    feat: update make-agent versions

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 import { DEPLOYMENT_URL } from "vercel-url";
 
-const ACCOUNT_ID = "rhea"
+const ACCOUNT_ID = "RHEA"
 
 // Set the plugin url in order of BITTE_CONFIG, env, DEPLOYMENT_URL (used for Vercel deployments)
 const PLUGIN_URL = DEPLOYMENT_URL || `${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.PORT || 3000}`;
