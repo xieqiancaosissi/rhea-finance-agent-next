b10be802041ee9efb0a5e76ad4b7b46417d9c813
naturexie <786281870@qq.com>
2025-04-26 21:05:45 +0800

    feat: update ACCOUNT_ID

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 import { DEPLOYMENT_URL } from "vercel-url";
 
-const ACCOUNT_ID = "juaner.near"
+const ACCOUNT_ID = "rhea-lending.near"
 
 // Set the plugin url in order of BITTE_CONFIG, env, DEPLOYMENT_URL (used for Vercel deployments)
 const PLUGIN_URL = DEPLOYMENT_URL || `${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.PORT || 3000}`;
