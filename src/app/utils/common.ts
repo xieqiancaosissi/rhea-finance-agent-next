a89c5539dac3eb2ed1fbbc6fdd879d89cb18208c
naturexie <786281870@qq.com>
2025-04-27 12:17:59 +0800

    feat: remove register logic

--- src/app/utils/common.ts
+++ src/app/utils/common.ts
@@ -1,6 +1,7 @@
 import { RHEA_LENDING_INTERFACE_DOMAIN } from "@/app/config";
 
 export async function register(account_id: string) {
+  return null;
   const query = await fetch(
     `${RHEA_LENDING_INTERFACE_DOMAIN}/storage_balance_of`,
     {
