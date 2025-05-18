0c7902d56686b95124272b3ca913b10cdeb099d5
naturexie <786281870@qq.com>
2025-05-18 08:33:50 +0800

    feat: update domian

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,4 @@
-export const INDEXER_DOMAIN_URL = "https://indexer.ref.finance";
+export const INDEXER_DOMAIN_URL = "https://api.ref.finance";
 export const BURROW_MAIN_CONTRACT_ID = "contract.main.burrow.near";
 export const WRAP_NEAR_CONTRACT_ID = "wrap.near";
 export const REF_FI_CONTRACT_ID = "v2.ref-finance.near";
